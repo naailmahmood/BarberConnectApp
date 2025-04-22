e48d46e15d28e7520f9b80bb51448e82ac594c46
naailmahmood <naail.r.mahmood@gmail.com>
2025-04-23 00:03:10 +0500

    Implemented SplashScreen

---
+++ app/src/main/java/com/example/firstapp/barberconnect/MainScreen.java
@@ -0,0 +1,39 @@
+package com.example.firstapp.barberconnect;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainScreen extends AppCompatActivity {
+    ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_screen);
+        logo = findViewById(R.id.logo);
+
+
+       // animation here
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.animation);
+        logo.startAnimation(anim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainScreen.this, HomeScreen.class));
+                logo.clearAnimation();
+                finish();
+            }
+        }, 5000);
+    }
+}
